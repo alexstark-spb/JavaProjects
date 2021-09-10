2db98d77f2ef90ce1388e11c1859da8be2bd3e7f
Alexander Derevyanko <70107243+alexstark-spb@users.noreply.github.com>
2021-09-10 12:17:36 +0300

    added update version

--- src/main/java/com/alexstark/Camera.java
+++ src/main/java/com/alexstark/Camera.java
@@ -15,17 +15,18 @@ public class Camera {
 
     void shootMovie(boolean famousOfActor1, boolean famousOfActor2) {
         System.out.println("Камера: Съёмка фильма с помощью " + numberOfMpx + " магапиксельной камеры типа - " + type);
-        if (famousOfActor1 || famousOfActor2) {
-            System.out.println("Для съёмок используется камера: " + cameraArray[2]);
-        } else {
-            System.out.println("Для съёмок используется камера: " + cameraArray[0]);
+        for (int i = 0; i < cameraArray.length; i++) {
+            if ((cameraArray[i].equals("Sony")) && (famousOfActor1 || famousOfActor2)) {
+                System.out.println("Один из актеров знаменит,поэтому для съёмок используется камера: Sony");
+            } else if (!(cameraArray[i].equals("Sony")) && (!(famousOfActor1 || famousOfActor2))) {
+                System.out.println("Данные актёры не слишком известны,поэтому для съёмок используется камера: " + cameraArray[i]);
+                break;
+            }
         }
     }
 
     void doAwaiting(){
         System.out.println("Камера: В ожидании...режиссировать некому, снимать нечего...");
     }
-
-
 
 }
--- src/main/java/com/alexstark/CinemaStudio.java
+++ src/main/java/com/alexstark/CinemaStudio.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class CinemaStudio {
     public static void main(String[] args) {
-        Actor actor1 = new Actor("male", "Макс", 31, "Спокойный", "Комедия", true);
-        Actor actor2 = new Actor("female", "Кейт", 25, "Импульсивная", "Мелодрама", false);
+        Actor actor1 = new Actor("male", "Макс", 31, "Спокойный", "Комедия", false);
+        Actor actor2 = new Actor("female", "Катя", 25, "Импульсивная", "Мелодрама", false);
         Director director = new Director("male", "Григорий", 55, "Неординарный", false);
         Stuntman stuntman = new Stuntman("male", "Фёдор", 29, "прыжки через огонь", 180, 85);
         Camera camera = new Camera("Большая", "Стационарная", 108);
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Выберите, будет ли режиссер пьян. Введите 'да' или 'нет':)");
-        String text = scanner.next();
+        String answer = scanner.next();
 
-        if (text.equals("да")) {
+        if (answer.equals("да")) {
             director.directFilm(true);
             camera.doAwaiting();
             actor1.expressOpinion1();
@@ -22,13 +22,27 @@ public class CinemaStudio {
             stuntman.waitingTrick();
             System.out.println("Съёмочная группа: Снимает пьяного режиссера в инстаграм :)");
 
-        } else if (text.equals("нет")) {
-            director.directFilm(false);
-            camera.shootMovie(actor1.isFamous, actor2.isFamous);
-            actor1.playRole();
-            actor2.playRole();
-            stuntman.performTrick();
-            System.out.println("Аплодисменты от съёмочной группы");
+        } else if (answer.equals("нет")) {
+            System.out.println("Выберите, будет ли один из актеров известен. Введите 'да' или 'нет'");
+            String answer2 = scanner.next();
+            if (answer2.equals("да")) {
+                actor1.isFamous = true;
+                director.directFilm(false);
+                camera.shootMovie(actor1.isFamous, actor2.isFamous);
+                actor1.playRole();
+                actor2.playRole();
+                stuntman.performTrick();
+                System.out.println("Аплодисменты от съёмочной группы");
+
+            } else if (answer2.equals("нет")) {
+                actor1.isFamous = false;
+                director.directFilm(false);
+                camera.shootMovie(actor1.isFamous, actor2.isFamous);
+                actor1.playRole();
+                actor2.playRole();
+                stuntman.performTrick();
+                System.out.println("Аплодисменты от съёмочной группы");
+            }
         }
     }
 }
